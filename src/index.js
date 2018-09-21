8c90b9ed386c37a31c0960e2b4310a1ed2e07ca6
Will Amaral <willian.amaral@outlook.com.br>
2018-09-21 04:34:33 -0300

    build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Remarkable from 'remarkable';
+import './main.scss';
 
 const md = new Remarkable();
+
 
 class App extends Component {
 	constructor(props) {
@@ -17,15 +19,40 @@ class App extends Component {
 
 	render() {
 		return (
-			<div className="container">
-				<div className="item">
-					<h1>Entrada</h1>
-					<p>Entre aqui seu texto em markdown</p>
-					<textarea name="entrada" id="entrada" cols="50" rows="15" onChange={this.handleChange}/>
+			<div className="hero is-fullheight">
+				<div className="hero-head">
+					<div className="content has-text-centered">
+						<h1 className="title is-1">Editor Markdown</h1>
+					</div>
 				</div>
-				<div className="item">
-					<h1>Saída</h1>
-					<div className="area" dangerouslySetInnerHTML={{__html: md.render(this.state.text)}}/>
+				<div className="hero-body">
+					<div className="container">
+						<div className="columns is-centered">
+							<div className="column">
+								<div className="content">
+									<h1 className="title">Entrada</h1>
+									<p className="subtitle">Entre aqui seu texto em markdown</p>
+								</div>
+								<textarea className="textarea is-danger" onChange={this.handleChange} />
+							</div>
+							<div className="column">
+								<div className="content">
+									<h1 className="title">Saída</h1>
+									<p className="subtitle">Código Markdown compilado</p>
+								</div>
+								<div className="box">
+									<div className="content" dangerouslySetInnerHTML={{__html: md.render(this.state.text)}}/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="hero-foot">
+					<footer className="footer">
+						<div className="content has-text-centered">
+							<p>Criado por <a href="https://will.amaral.io">Willian Amaral</a></p>
+						</div>
+					</footer>
 				</div>
 			</div>
 		);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	module: {
@@ -7,10 +8,19 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: {loader: 'babel-loader'}
+			},
+			{
+				test: /\.(css|sass|scss)$/,
+				use: [
+					process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
 			}
 		]
 	},
 	plugins: [
-		new HtmlWebpackPlugin({template: './src/index.html'})
+		new HtmlWebpackPlugin({template: './src/index.html'}),
+		new MiniCssExtractPlugin()
 	]
 };
